5ae7a42bb72ccaedf90f0e76572fbec8160e6404
Robert Cunningham <rgc3dev@gmail.com>
2016-02-19 08:25:20 -0500

    Add feed, edit navbar

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,10 +10,17 @@ class ProfilesController < ApplicationController
   end
 
   # Added def create and def user_params for paperclip
-  
+
   def create
   	@user = User.create( user_params )
 	end
+
+  def feed
+    @tweets = Tweet.where("user_id in (?) OR user_id = ?",
+    current_user.friend_ids, current_user).order('created_at DESC')
+
+    @tweet = Tweet.new
+  end
 
 	private
 
