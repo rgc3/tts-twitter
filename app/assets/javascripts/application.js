3676db97176050e8bab37058bbcf3e912131bf31
Robert Cunningham <rgc3dev@gmail.com>
2016-02-19 08:42:48 -0500

    add jquery to follow and unfollow buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,15 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+  $('#unfollow_btn').hover(function(){
+    $(this).removeClass('btn-primary')
+    $(this).addClass('btn-danger')
+    $(this).html('Unfollow')
+  }, function(){
+    $(this).html('Following')
+    $(this).removeClass('btn-danger')
+    $(this).addClass('btn-primary')
+  });
+});
