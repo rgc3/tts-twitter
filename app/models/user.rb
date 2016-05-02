786724b367106b67c0e5a8d33cf15e703f340162
Robert Cunningham <rgc3dev@gmail.com>
2016-05-02 08:48:23 -0400

    Fix bug. Add users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_attached_file :avatar, styles: { medium: "300x300>", small: "200x200>", thumb: "100x100>", tiny: "32x32>" }, default_url: "/images/:style/missing.png"
-
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   has_many :tweets
